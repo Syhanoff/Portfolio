29fc8ec0d2c52961eb8ae53bb3b042a9bb620756
Syhanoff <s.syhanof@gmail.com>
2022-03-31 12:54:04 +0300

    Адаптация последнего экрана

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,6 +1,6 @@
 // Пример
 // import './modules/slider';
-// import './modules/tabs';
+
 import './modules/hamburger';
 import './modules/skillsline';
 import './modules/mouse';
--- source/js/_libs.js
+++ source/js/_libs.js
@@ -1,6 +1,5 @@
 // Пример импорта изпапки
 // import './libs/slick.min.js';
-// import './libs/wow.min.js';
 
 // Пример импорта из node-модулей
 // import $ from "jquery";
